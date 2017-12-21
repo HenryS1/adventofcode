90292fd00ef1d9a0d7165021b5e0fa0550c28919
Henry Steere <henry.steere@gmail.com>
2017-12-21 23:14:04 +0200

    solved part 1 of fractal art

---
+++ 2017/fractal-art.lisp
@@ -0,0 +1,156 @@
+(ql:quickload 'cl-ppcre)
+
+(defun apply-pattern (row column pattern grid size)
+;  (format t "apply size blah blah ~a ~a ~a~% ~a~%" row column (+ size 1) pattern)
+  (loop for r from row to (+ row size)
+     do (loop for c from column to (+ column size)
+           do (progn 
+;                (format t "r c ~a ~a~%" r c)
+                (setf (aref grid r c)
+                    (get-entry r c pattern (+ size 1)))))))
+
+(defun get-entry (row column pattern size)
+  ;; (format t "entry ~a ~a ~a~%" (mod row size) (mod column size)
+  ;;         (aref pattern (+ (* (mod row size) size) (mod column size))))
+  (aref pattern (+ (* (mod row size) size) (mod column size))))
+
+(defun rotate-3-key (key)
+  (destructuring-bind (a b c 
+                        d e f
+                        g h i) key
+    (list g d a h e b i f c)))
+
+(defun rotate-2-key (key)
+  (destructuring-bind (a b c d) key
+    (list c a d b)))
+
+(defun rotate-key (key)
+  (if (= (length key) 9)
+      (rotate-3-key key)
+      (rotate-2-key key)))
+
+(defun flip-2-key (key)
+  (destructuring-bind (a b 
+                       c d) key
+    (list b a d c)))
+
+(defun flip-3-key (key)
+  (destructuring-bind (a b c
+                       d e f
+                       g h i) key
+    (list c b a f e d i h g)))
+
+(defun flip-key (key)
+  (if (= (length key) 9)
+      (flip-3-key key)
+      (flip-2-key key)))
+
+(defun lookup-pattern (grid row col size patterns)
+  (let* ((key (make-key grid row col size))
+         (flipped (flip-key key)))
+;    (format t "key ~a~%" key)
+    (loop until (or (gethash key patterns)
+                    (gethash flipped patterns))
+       do (progn (setf key (rotate-key key))
+                 (setf flipped (rotate-key flipped))))
+    (if (gethash key patterns)
+        (format t "~a~%" key)
+        (format t "~a~%" flipped))
+    (or (gethash key patterns)
+        (gethash flipped patterns))))
+
+(defun make-key (grid row col size)
+  (let (key)
+    (loop for r from row to (+ row (- size 1))
+       do (loop for c from col to (+ col (- size 1))
+             do (push (aref grid r c) key)))
+    (reverse key)))
+
+(defun expand-pattern (grid new-grid patterns size)
+  (format t "here0~%")
+    (destructuring-bind (rows columns) (array-dimensions grid)
+      (format t "rows ~a~%" rows)
+      (format t "size ~a~%" size)
+      (loop for r = 0 then (+ r size) 
+         while (< r (- rows 1))
+         do (loop for c = 0 then (+ c size)
+               while (< c (- columns 1))
+               do (let* ((pattern (lookup-pattern grid r c size patterns))
+                         (new-r (* (+ size 1) (floor r size)))
+                         (new-c (* (+ size 1) (floor c size))))
+                    (apply-pattern new-r new-c
+                                   pattern new-grid 
+                                   size))))
+      new-grid))
+
+(defun alternate-size (current)
+  (if (= current 2) 3 2))
+
+(defun convert-coord (coord size new-size)
+  (* (floor coord size) (floor new-size size)))
+
+(defun make-next-grid (r)
+  (if (= (mod r 2) 0)
+      (let ((dim (+ (floor r 2) r)))
+        (make-array (list dim dim) :initial-element #\.))
+      (let ((dim (+ (floor r 3) r)))
+        (make-array (list dim dim) :initial-element #\.))))
+
+(defun expand-grid (grid patterns)
+  (destructuring-bind (r c) (array-dimensions grid)
+    (declare (ignorable c))
+    (let ((next-grid (make-next-grid r)))
+      (if (= (mod r 2) 0)
+          (expand-pattern grid next-grid patterns 2)
+          (expand-pattern grid next-grid patterns 3)))))
+
+(defun parse-pattern (line)
+  (let ((syms (cl-ppcre:split " " line)))
+    (list (remove-if (lambda (c) (char= c #\/)) 
+                     (loop for c across (car syms) collect c))
+          (remove-if (lambda (c) (char= c #\/)) (caddr syms)))))
+
+(defun initial-pattern ()
+  (make-array '(3 3) :initial-contents 
+              '((#\. #\# #\.)
+                (#\. #\. #\#)
+                (#\# #\# #\#))))
+
+(defun process-lines (filename callback)
+  (with-open-file (f filename)
+    (when f
+      (loop for line = (read-line f nil nil)
+         while line
+         do (funcall callback line)))))
+
+(defun get-patterns (filename)
+  (let ((patterns (make-hash-table :test 'equal)))
+    (process-lines filename (lambda (line)
+                     (let ((pattern (parse-pattern line)))
+                       (setf (gethash (car pattern) patterns) (cadr pattern)))))
+    patterns))
+
+(defun real-patterns ()
+  (get-patterns "fractal-art-input.txt"))
+
+(defun test-patterns ()
+  (get-patterns "fractal-art-test-input"))
+
+(defun count-turned-on (grid)
+  (let ((total 0))
+    (destructuring-bind (r c) (array-dimensions grid)
+      (declare (ignorable c))
+      (loop for i from 0 to (- r 1)
+         do (loop for j from 0 to (- r 1)
+               when (char= (aref grid i j) #\#)
+               do (incf total))))
+    total))
+
+(defun expand (iterations patterns)
+  (labels ((recur (remaining grid)
+             (format t "~a~%" grid)
+             (if (= remaining 0)
+                 grid
+                 (recur (- remaining 1) (expand-grid grid patterns)))))
+    (let ((result (recur iterations (initial-pattern))))
+      (count-turned-on result))))
--- 2017/particle-swarm.lisp
+++ 2017/particle-swarm.lisp
@@ -2,7 +2,7 @@
 
 (defun parse-particle (line)
   (multiple-value-bind (match registers)
-      (cl-ppcre:scan-to-strings "[^\\d]+(-?\\d+),(-?\\d+),(-?\\d+)[^\\d]+(-?\\d+),(-?\\d+),(-?\\d+)[^\\d]+(-?\\d+),(-?\\d+),(-?\\d+)[^\\d]+" line)
+      (cl-ppcre:scan-to-strings "[^\\d-]+(-?\\d+),(-?\\d+),(-?\\d+)[^\\d-]+(-?\\d+),(-?\\d+),(-?\\d+)[^\\d-]+(-?\\d+),(-?\\d+),(-?\\d+)[^\\d]+" line)
     (declare (ignorable match))
     (map 'list #'parse-integer registers)))
 
@@ -37,8 +37,68 @@
       (loop for line = (read-line f nil nil)
          while line collect (parse-particle line)))))
 
+(defun update-particle (particle)
+  (destructuring-bind (px py pz vx vy vz ax ay az) particle
+    (let ((vx+ (+ vx ax))
+          (vy+ (+ vy ay))
+          (vz+ (+ vz az)))
+      (list (+ px vx+) (+ py vy+) (+ pz vz+)
+            vx+ vy+ vz+
+            ax ay az))))
+
+(defun update-positions (particles)
+  (if (null particles)
+      nil
+      (cons (update-particle (car particles))
+            (update-positions (cdr particles)))))
+
+(defun take-3 (particle)
+  (list (car particle) (cadr particle) (caddr particle)))
+
+(defun has-collisions (particles)
+  (let ((positions (make-hash-table :test 'equal)))
+    (format t "~a~%" particles)
+    (loop for p in particles
+       do (let ((key (take-3 p)))
+            (format t "~a~%" key)
+            (if (gethash key positions)
+                (return-from has-collisions t)
+                (setf (gethash key positions) t))))
+    nil))
+
+(defun remove-collisions (particles)
+  (let ((positions (make-hash-table :test 'equal)))
+    (loop for p in particles
+       do (let ((key (take-3 p)))
+            (if (gethash key positions)
+                (incf (gethash key positions))
+                (setf (gethash key positions) 1))))
+    (remove-if (lambda (p) (> (gethash (take-3 p) positions) 1)) particles)))
+
+(defun loop-until-collision (particles)
+  (loop until (has-collisions particles)
+     do (setf particles (update-positions particles)))
+  particles)
+
+(defun test-part-2 ()
+  (loop-until-collision (test-points)))
+
+(defun first-collision-real-points ()
+  (loop-until-collision (real-points)))
+
+(defun loop-and-remove-collisions (particles iterations)
+  (loop for i from 1 to iterations
+     do (setf particles (remove-collisions (update-positions particles))))
+  particles)
+
+(defun loop-and-remove-test (iterations)
+  (loop-and-remove-collisions (test-points) iterations))
+
+(defun loop-and-remove-real (iterations)
+  (loop-and-remove-collisions (real-points) iterations))
+
 (defun test-points ()
-  (collect-points "particle-swarm-test-input"))
+  (collect-points "particles-test-input-2"))
 
 (defun real-points ()
   (collect-points "particle-swarm-input.txt"))
