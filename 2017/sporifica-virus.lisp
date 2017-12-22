b84f1e903872198f0956717e201766b4c360e746
Henry Steere <henry.steere@gmail.com>
2017-12-22 18:10:12 +0200

    finished part 2 of sporifica

--- 2017/sporifica-virus.lisp
+++ 2017/sporifica-virus.lisp
@@ -14,6 +14,14 @@
                          (east 'north))))
     (cons new-direction (cdr node))))
 
+(defun turn-back (node)
+  (let ((new-direction (case (car node)
+                         (north 'south)
+                         (west 'east)
+                         (south 'north)
+                         (east 'west))))
+    (cons new-direction (cdr node))))
+
 (defun adjust-direction (current grid)
   (if (gethash (cdr current) grid)
       (turn-right current)
@@ -26,6 +34,30 @@
         (funcall callback)
         (setf (gethash (cdr current) grid) t))))
 
+(defun get-infection-status (current grid)
+  (let ((lookup (gethash (cdr current) grid)))
+    (if lookup lookup 'C)))
+
+(defun cycle-infection-status (current grid callback)
+  (let ((status (get-infection-status current grid)))
+    (let ((next-status (case status
+                         (C 'W)
+                         (W 'I)
+                         (I 'F)
+                         (F 'C))))
+      (if (eq next-status 'I)
+          (funcall callback))
+      (setf (gethash (cdr current) grid) next-status))))
+
+(defun adjust-direction-updated (current grid)
+  (let ((status (get-infection-status current grid)))
+;    (format t "status ~a~%" status)
+    (case status
+      (C (turn-left current))
+      (W current)
+      (I (turn-right current))
+      (F (turn-back current)))))
+
 (defun move-node (current)
   (let ((position (cdr current)))
     (let ((new-position (case (car current)
@@ -35,11 +67,18 @@
                           (west (cons (car position) (- (cdr position) 1))))))
       (cons (car current) new-position))))
 
-(defun update (current grid callback)
+(defun iter (current grid callback)
   (progn 
-;    (format t "~a~%" current)
     (setf current (adjust-direction current grid))
     (update-infection-status current grid callback)
+    (move-node current)))
+
+(defun iter-updated (current grid callback)
+  (progn 
+;    (format t "~a ~a~%" current grid)
+    (setf current (adjust-direction-updated current grid))
+;    (format t "current updated ~a~%" current)
+    (cycle-infection-status current grid callback)
     (move-node current)))
 
 (defun collect-lines (filename)
@@ -65,7 +104,7 @@
     (loop for r from 0 to (- rows 1)
        do (loop for c from 0 to (- cols 1)
              when (char= (aref (aref lines r) c) #\#)
-             do (setf (gethash (cons r c) grid) t)))
+             do (setf (gethash (cons r c) grid) 'I)))
     (values (cons 'north (cons (center rows) (center cols))) grid)))
 
 (defun part-1-solver (lines iterations)
@@ -74,9 +113,19 @@
       (labels ((recur (current iterations)
                  (if (> iterations 0)
                      (let ((new-current 
-                            (update current grid (lambda () (incf infection-events)))))
+                            (iter current grid (lambda () (incf infection-events)))))
                        (recur new-current (- iterations 1))))))
-;        (format t "start ~a~%" start)
+        (recur start iterations)
+        infection-events))))
+
+(defun part-2-solver (lines iterations)
+  (multiple-value-bind (start grid) (build-grid lines)
+    (let ((infection-events 0))
+      (labels ((recur (current iterations)
+                 (if (> iterations 0)
+                     (let ((new-current
+                            (iter-updated current grid (lambda () (incf infection-events)))))
+                       (recur new-current (- iterations 1))))))
         (recur start iterations)
         infection-events))))
 
@@ -85,3 +134,9 @@
 
 (defun solution-part-1 ()
   (part-1-solver (real-lines) 10000))
+
+(defun test-part-2 ()
+  (part-2-solver (test-lines) 10000000))
+
+(defun solution-part-2 ()
+  (part-2-solver (real-lines) 10000000))
