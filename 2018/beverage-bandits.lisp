bda0fa45b27c8c4281a738cb0b6dab593ef3d6fc
Henry Steere <henry.steere@gmail.com>
2018-12-15 22:30:59 +0200

    Fixed subtle bug

--- 2018/beverage-bandits.lisp
+++ 2018/beverage-bandits.lisp
@@ -16,9 +16,8 @@
 
 (defparameter *test-input-file* "beverage-bandits-test-input.txt")
 
-(defun read-map (filename)
-  (let ((text-map (read-lines filename))
-        (coord-map (make-hash-table :test 'equal))
+(defun parse-map (text-map)
+  (let ((coord-map (make-hash-table :test 'equal))
         (units (make-hash-table :test 'equal)))
     (loop for text-row in text-map
        for row = 0 then (1+ row)
@@ -36,6 +35,21 @@
     (loop for coord being the hash-keys of coord-map
        do (setf (gethash coord coord-map) (reverse (gethash coord coord-map))))
     (values coord-map units)))
+
+(defun print-map (coord-map units)
+  (loop for row from 0 to 31
+     do (loop for col from 0 to 31 
+           do (let ((coord (cons row col)))
+                (if (gethash coord units)
+                    (format t "~a" (car (gethash coord units)))
+                    (if (gethash coord coord-map)
+                        (format t ".")
+                        (format t "#")))))
+       (format t "~%")))
+
+(defun read-map (filename)
+  (let ((text-map (read-lines filename)))
+    (parse-map text-map)))
 
 (defun add-neighbours (coord coord-map)
   (setf (gethash coord coord-map) nil)
@@ -59,30 +73,45 @@
       coord
       (first-step start (gethash coord path-to) path-to)))
 
-(defun find-place-to-move (unit start coord-map units)
-  (let ((q (make-queue))
-        (seen (make-hash-table :test 'equal))
-        (path-to (make-hash-table :test 'equal)))
-    (labels ((bfs (coord)
-               (loop for neighbour in (neighbours coord coord-map)
-                  do (let ((possible-unit (gethash neighbour units)))
-                       (when (and possible-unit (is-enemy unit possible-unit))
-                         (return-from find-place-to-move (first-step start coord path-to))))
-                  when (and (not (gethash neighbour seen))
-                            (not (gethash neighbour units)))
-                  do (setf (gethash neighbour seen) t)
-                    (setf (gethash neighbour path-to) coord)
-                    (enqueue neighbour q))
-               (let ((next (poll q)))
-                 (when next
-                   (bfs next)))))
-      (bfs start)
-      start)))
-
 (defun reading-order (one other)
   (or (< (car one) (car other))
       (and (= (car one) (car other))
            (< (cdr one) (cdr other)))))
+
+(defun find-place-to-move (unit start coord-map units)
+  (let ((q (make-queue))
+        (seen (make-hash-table :test 'equal))
+        (path-to (make-hash-table :test 'equal))
+        (distance-to (make-hash-table :test 'equal))
+        (min-distance 999999999)
+        closest)
+    (setf (gethash start distance-to) 0)
+    (labels ((bfs (coord)
+               ;; (format t "BFS ~a~%" coord)
+               ;; (format t "QUEUE ~a~%" q)
+               ;; (format t "CLOSEST ~a~%" closest)
+               ;; (when (equal start (cons 6 19))
+               ;;   (format t "EXPLORING AT ~a DISTANCE ~a~%" coord (gethash coord distance-to)))
+               (loop for neighbour in (neighbours coord coord-map)
+                  do (let ((possible-unit (gethash neighbour units)))
+                       (when (and possible-unit (is-enemy unit possible-unit))
+                         ;; (format t "FOUND UNIT WHEN AT ~a FROM START ~a~%" coord start)
+                         (setf min-distance (gethash coord distance-to))
+                         (push coord closest)))
+                  when (and (not (gethash neighbour seen))
+                            (not (gethash neighbour units)))
+                  do (setf (gethash neighbour seen) t)
+                    (setf (gethash neighbour path-to) coord)
+                    (setf (gethash neighbour distance-to) (1+ (gethash coord distance-to)))
+                    (enqueue neighbour q))
+               (let ((next (poll q)))
+                 (when (and next (<= (gethash next distance-to) min-distance))
+                   (bfs next)))))
+      (bfs start)
+ ;;     (format t "START ~a CLOSEST ~a~%" start closest)
+      (if closest
+          (first-step start (car (sort closest #'reading-order)) path-to)
+          start))))
 
 (defun compare-hit-points (units)
   (lambda (one other)
@@ -121,21 +150,23 @@
            (return-from has-enemies t)))
     nil))
 
-(defun tick-round (coord-map units start-round elf-attack-power)
+(defun tick-round (coord-map units start-round elf-attack-power &optional (return-early nil))
   (let ((unit-order (sort (hash-keys units) #'reading-order))
-        elf-died)
+        elf-died
+        (moved-to (make-hash-table :test 'equal)))
     (loop for coord in unit-order
        when (not (has-enemies units))
        do (return-from tick-round (values start-round nil))
-       when (gethash coord units)
+       when (and (gethash coord units) (not (gethash coord moved-to)))
        do (let ((first-step (find-place-to-move (gethash coord units) coord coord-map units)))
             (move first-step (gethash coord units) coord units)
+            (setf (gethash first-step moved-to) t)
             (let ((target-coord (find-adjacent-enemy
                                  (gethash first-step units) first-step units coord-map)))
               (when target-coord
                 (setf elf-died (attack (gethash target-coord units) 
                                        target-coord units elf-attack-power)))))
-       when elf-died
+       when (and elf-died return-early)
        do (return-from tick-round (values (1+ start-round) t)))
     (values (1+ start-round) nil)))
 
@@ -153,13 +184,19 @@
   (multiple-value-bind (coord-map units) (read-map *test-input-file*)
     (let ((final-round (tick-until-battle-is-finished coord-map units))
           (total-health (total-health units)))
+      (format t "ROUND ~a TOTAL-HEALTH ~a~%" final-round total-health)
       (* final-round total-health))))
 
 (defun solution-part-1 ()
   (multiple-value-bind (coord-map units) (read-map *input-file*)
+    ;; (print-map coord-map units)
+    ;; (format t "~%")
+    ;; (tick-round coord-map units 0 3)
+    ;; (print-map coord-map units)
     (let ((final-round (tick-until-battle-is-finished coord-map units))
           (total-health (total-health units)))
-      (* final-round total-health))))
+      (* final-round total-health))
+    ))
 
 (defun copy-units (units)
   (let ((new-units (make-hash-table :test 'equal)))
