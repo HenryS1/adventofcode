c8734dc90ab2f8d61a8a15e513ddd08ee43b4dbf
Henry Steere <henry.steere@gmail.com>
2018-12-25 19:12:28 +0200

    Day 23

--- 2018/experimental-emergency-teleportation.lisp
+++ 2018/experimental-emergency-teleportation.lisp
@@ -1,6 +1,8 @@
 (load "utilities.lisp")
+(load "priority-queue.lisp")
 (ql:quickload :cl-ppcre)
 
+(defparameter *test-input-file* "experimental-emergency-teleportation-test-input.txt")
 (defparameter *input-file* "experimental-emergency-teleportation-input.txt")
 
 (defun read-nano-bot (line)
@@ -48,183 +50,132 @@
      maximize (aref bot 2) into max-z
      finally (return (vector min-x max-x min-y max-y min-z max-z))))
 
-(defun find-cluster-bounding-box (cluster)
-  (loop for bot being the hash-keys of cluster 
-     minimize (aref bot 0) into min-x
-     minimize (aref bot 1) into min-y
-     minimize (aref bot 2) into min-z
-     maximize (aref bot 0) into max-x
-     maximize (aref bot 1) into max-y
-     maximize (aref bot 2) into max-z
-     finally (return (vector min-x max-x min-y max-y min-z max-z))))
-
-(defun range-intersects (one other)
-  (< (manhattan-distance one other)
-     (+ (signal-strength one) (signal-strength other))))
-
 (defun equal-bots (one other)
   (every #'= one other))
 
-(defun find-cluster (bot nano-bots)
-  (loop with cluster = (let ((table (make-hash-table))) 
-                         (setf (gethash bot table) t)
-                         table)
-     for other across nano-bots
-     when (and (not (equal-bots bot other))
-               (range-intersects bot other))
-     do (setf (gethash other cluster) t)
-     finally (return cluster)))
+(defun min-x (bounding-box)
+  (aref bounding-box 0))
 
-(defun find-clusters (nano-bots)
-  (loop with clusters = nil
-     for bot across nano-bots
-     when (not (some (lambda (cluster) (gethash bot cluster)) clusters))
-     do (push (find-cluster bot nano-bots) clusters)
-     finally (return clusters)))
+(defun max-x (bounding-box)
+  (aref bounding-box 1))
 
-(defun find-best-cluster (clusters)
-  (loop with best = nil
-     with best-size = 0
-     for cluster in clusters
-     when (> (hash-table-count cluster) best-size)
-     do (setf best-size (hash-table-count cluster)
-              best cluster)
-     finally (return best)))
+(defun min-y (bounding-box)
+  (aref bounding-box 2))
 
-(defun up (point step-size bounding-box)
-  (vector (aref point 0) (aref point 1) (min (+ (aref point 2) step-size) 
-                                             (aref bounding-box 5))))
+(defun max-y (bounding-box)
+  (aref bounding-box 3))
 
-(defun down (point step-size bounding-box)
-  (vector (aref point 0) (aref point 1) (max (- (aref point 2) step-size)
-                                             (aref bounding-box 4))))
+(defun min-z (bounding-box)
+  (aref bounding-box 4))
 
-(defun north (point step-size bounding-box)
-  (vector (min (+ (aref point 0) step-size)
-               (aref bounding-box 1))
-          (aref point 1) (aref point 2)))
+(defun max-z (bounding-box)
+  (aref bounding-box 5))
 
-(defun south (point step-size bounding-box)
-  (vector (max (- (aref point 0) step-size)
-               (aref bounding-box 0))
-          (aref point 1) (aref point 2)))
+(defun x (bot) (aref bot 0))
+(defun y (bot) (aref bot 1))
+(defun z (bot) (aref bot 2))
 
-(defun east (point step-size bounding-box)
-  (vector (aref point 0) (min (+ (aref point 1) step-size)
-                              (aref bounding-box 3))
-          (aref point 2)))
+(defun inside-bounding-box (bot bounding-box)
+  (and (<= (min-x bounding-box) (x bot) (max-x bounding-box))
+       (<= (min-y bounding-box) (y bot) (max-y bounding-box))
+       (<= (min-z bounding-box) (z bot) (max-z bounding-box))))
 
-(defun west (point step-size bounding-box)
-  (vector (aref point 0) (max (- (aref point 1) step-size)
-                              (aref bounding-box 2))
-          (aref point 2)))
+(defun distance-to-bbox (bot bounding-box)
+  (+ (if (<= (min-x bounding-box) (x bot) (max-x bounding-box)) 0 
+         (min (abs (- (x bot) (min-x bounding-box))) (abs (- (x bot) (max-x bounding-box)))))
+     (if (<= (min-y bounding-box) (y bot) (max-y bounding-box)) 0
+         (min (abs (- (y bot) (min-y bounding-box))) (abs (- (y bot) (max-y bounding-box)))))
+     (if (<= (min-z bounding-box) (z bot) (max-z bounding-box)) 0
+         (min (abs (- (z bot) (min-z bounding-box))) (abs (- (z bot) (max-z bounding-box)))))))
 
-(defun neighbours (step-size point bounding-box)
-  (vector (up point step-size bounding-box) (down point step-size bounding-box)
-          (north point step-size bounding-box) (south point step-size bounding-box)
-          (east point step-size bounding-box) (west point step-size bounding-box)))
+(defun bot-in-range (bot bounding-box)
+  (<= (distance-to-bbox bot bounding-box) (signal-strength bot)))
 
-(defun point-in-range (point bot)
-  (< (manhattan-distance point bot)
-     (signal-strength bot)))
+(defun make-space (bounding-box bots)
+  (let* ((intersections (count-if (lambda (bot) (bot-in-range bot bounding-box)) bots))
+         (size (+ (- (max-x bounding-box) (min-x bounding-box))
+                  (- (max-y bounding-box) (min-y bounding-box))
+                  (- (max-z bounding-box) (min-z bounding-box))))
+         (distance-to-origin (+ (abs (min-x bounding-box)) 
+                                (abs (min-y bounding-box)) 
+                                (abs (min-z bounding-box)))))
+    (list intersections size bounding-box distance-to-origin)))
 
-(defun intersection-count (cluster)
-  (lambda (point)
-    (loop for bot being the hash-keys of cluster
-                   count (point-in-range point bot))))
+(defun size (space) (cadr space))
+(defun intersections (space) (car space))
+(defun get-bounding-box (space) (caddr space))
+(defun distance-to-origin (space) (cadddr space))
 
-(defun sample-distribution (distribution distribution-total)
-  (loop with selection = (random distribution-total)
-     for entry across distribution
-     when (> (car entry) selection)
-     return (cdr entry)))
+(defun space-ordering (one other)
+  (or (> (intersections one)
+         (intersections other))
+      (and (= (intersections one)
+              (intersections other))
+           (or (< (distance-to-origin one) (distance-to-origin other))
+               (and (= (distance-to-origin one) (distance-to-origin other))
+                    (< (size one) (size other)))))))
 
-(defun find-next-point (step-size point cluster bounding-box better-weight worse-weight)
-  (loop with nbrs = (map 'vector (lambda (point) 
-                                   (cons (funcall (intersection-count cluster) point) point)) 
-                         (neighbours step-size point bounding-box))
-     with start-intersections = (funcall (intersection-count cluster) point)
-     with distribution = (make-array (length nbrs) :fill-pointer 0 :adjustable t)
-     with distribution-total = 0
-     for neighbour across nbrs
-     when (> (car neighbour) start-intersections)
-     do (incf distribution-total (* (car neighbour) better-weight))
-     when (<= (car neighbour) start-intersections)
-     do (incf distribution-total (* (car neighbour) worse-weight))
-     do (vector-push-extend (cons distribution-total (cdr neighbour)) distribution)
-     finally (return (sample-distribution distribution distribution-total))))
+(defun split-bounding-box (bounding-box)
+  (let ((mid-x (floor (+ (min-x bounding-box) (max-x bounding-box)) 2))
+        (mid-y (floor (+ (min-y bounding-box) (max-y bounding-box)) 2))
+        (mid-z (floor (+ (min-z bounding-box) (max-z bounding-box)) 2)))
+    (vector (vector (min-x bounding-box) mid-x
+                    (min-y bounding-box) mid-y
+                    (min-z bounding-box) mid-z)
+            (vector (min-x bounding-box) mid-x
+                    (min-y bounding-box) mid-y
+                    mid-z (max-z bounding-box))
+            (vector (min-x bounding-box) mid-x
+                    mid-y (max-y bounding-box)
+                    (min-z bounding-box) mid-z)
+            (vector (min-x bounding-box) mid-x
+                    mid-y (max-y bounding-box)
+                    mid-z (max-z bounding-box))
+            (vector mid-x (max-x bounding-box)
+                    (min-y bounding-box) mid-y
+                    (min-z bounding-box) mid-z)
+            (vector mid-x (max-x bounding-box)
+                    (min-y bounding-box) mid-y
+                    mid-z (max-z bounding-box))
+            (vector mid-x (max-x bounding-box)
+                    mid-y (max-y bounding-box)
+                    (min-z bounding-box) mid-z)
+            (vector mid-x (max-x bounding-box)
+                    mid-y (max-y bounding-box)
+                    mid-z (max-z bounding-box)))))
 
-(defun closer-to-origin (one other)
-  (< (manhattan-distance one #(0 0 0)) (manhattan-distance other #(0 0 0))))
+(defun partition (bounding-box bots)
+  (let ((sub-spaces (split-bounding-box bounding-box)))
+    (map 'vector (lambda (sub-space) (make-space sub-space bots)) sub-spaces)))
 
-(defun find-best-intersection (points cluster)
-  (loop with best-count = 0
-     with best = nil
-     with intersection-fun = (intersection-count cluster)
-     for point across points
-     for intersections = (funcall intersection-fun point)
-     when (or (> intersections best-count)
-              (and  
-               (= intersections best-count)
-               (closer-to-origin point best)))
-     do (setf best-count intersections)
-       (setf best point)
-     finally (return (cons best best-count))))
+(defun enqueue-subspaces (space q bots)
+  (loop with sub-spaces = (let ((spaces (partition (get-bounding-box space) bots)))
+                            spaces)
+     for sp across sub-spaces
+      do (insert-pq sp q)))
 
-(defun maximize-intersections-for-cluster (cluster)
-  (maximize-intersections cluster (coerce (hash-keys cluster) 'vector)))
+(defun find-best-coordinate (bounding-box bots)
+  (let ((q (make-pq #'space-ordering)))
+    (insert-pq (make-space bounding-box bots) q)
+    (loop with seen = (make-hash-table :test 'equal)
+       for space = (pop-pq q)
+       for key = (map 'list #'identity (get-bounding-box space))
+       while (and space (> (size space) 0))
+       when (not (gethash key seen))       
+       do (enqueue-subspaces space q bots)
+         (setf (gethash key seen) t)
+       finally (return space))))
 
-(defun maximize-intersections (cluster starting-points)
-  (loop with best-intersections = (reduce #'max (map 'vector (intersection-count cluster)
-                                                        starting-points))
-     with best-point = nil
-     with bounding-box = (find-bounding-box starting-points)
-     with step-size = (floor (max (- (aref bounding-box 1) (aref bounding-box 0))
-                                  (- (aref bounding-box 3) (aref bounding-box 2))
-                                  (- (aref bounding-box 5) (aref bounding-box 4)))
-                             2) 
-     for better-weight = 100
-     with worse-weight = 50
-     for points = starting-points 
-     then (map 'vector (lambda (point) (find-next-point (max 3 (random step-size)) point cluster 
-                                                        bounding-box better-weight worse-weight))
-               points)
-     for i from 1 to 3000
-;;     do (format t "POINTS ~a~%" points)
-     when (= (mod i 10) 0)
-     do 
-       (destructuring-bind (point . intersections) (find-best-intersection points cluster)
-         (format t "INTERSECTIONS ~a ~a~%" intersections point)
-         (format t "BEST INTERSECTIONS ~a~%" best-intersections)
-         (when (or (> intersections best-intersections)
-                   (and (= intersections best-intersections)
-                        (or (not best-point)
-                            (closer-to-origin point best-point))))
-           (format t "NEW BEST ~a ~a~%" intersections point)
-           (setf best-point point
-                 best-intersections intersections)
-           (maximize-intersections-with-starting-neighbourhood cluster point)))
-     finally (return best-intersections)))
+(defun test-2 ()
+  (let* ((bots (read-input *test-input-file*))
+         (bounding-box (find-bounding-box bots)))
+    (find-best-coordinate bounding-box bots)))
 
-(defun maximize-intersections-with-starting-neighbourhood (cluster point)
-  (let ((starting-points (create-neighbourhood point cluster)))
-    (maximize-intersections cluster starting-points)))
+(defun solution-part-2 ()
+  (let* ((bots (read-input *input-file*))
+         (bounding-box (find-bounding-box bots)))
+    (find-best-coordinate bounding-box bots)))
 
-(defun create-neighbourhood (center cluster)
-  (coerce (loop with bounding-box = (find-cluster-bounding-box cluster)
-             with points = (vector center)
-             for i from 1 to 500
-             do (setf points (concatenate 
-                              'vector points (neighbours (random 1000000) center bounding-box)))
-             finally (return points)) 
-          'vector))
-
-(defun find-closest-to-origin (starting-point cluster intersections)
-  (loop with previous = starting-point
-     with bb = (find-cluster-bounding-box cluster) 
-     for point = starting-point then (west (south (down point 1 bb) 1 bb) 1 bb)
-     do (format t "INTERSECTIONS ~a ~a~%" point (funcall (intersection-count cluster) point))
-     when (= intersections (funcall (intersection-count cluster) point))
-     do (setf previous point)
-     until (> intersections (funcall (intersection-count cluster) point))
-     finally (return previous)))
+(defun find-bots-in-range (point bots)
+  (loop for bot across bots 
+     count (<= (manhattan-distance bot point) (signal-strength bot))))
