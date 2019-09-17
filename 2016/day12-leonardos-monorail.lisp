0c2a4520c0d43d6ee5db3418468eb8a549ca794e
Henry Steere <henry@jemstep.com>
2019-09-17 10:33:13 +0200

    Compile to one function

--- 2016/day12-leonardos-monorail.lisp
+++ 2016/day12-leonardos-monorail.lisp
@@ -13,45 +13,45 @@
     (d 3)
     (otherwise s)))
 
-(defun generate-copy (args)
+(defun generate-copy (instruction-index args)
   (let ((translated-syms (mapcar #'translate-sym args)))
     (if (symbolp (car args))
-        `(lambda ()
-           (setf (aref *registers* ,(cadr translated-syms)) 
-                 (aref *registers* ,(car translated-syms)))
-           (incf *instruction-pointer*))
-        `(lambda ()
-           (setf (aref *registers* ,(cadr translated-syms))
-                 ,(car translated-syms))
-           (incf *instruction-pointer*)))))
+        `(,instruction-index
+           (progn (setf (aref *registers* ,(cadr translated-syms)) 
+                        (aref *registers* ,(car translated-syms)))
+                  (incf *instruction-pointer*)))
+        `(,instruction-index
+          (progn (setf (aref *registers* ,(cadr translated-syms))
+                       ,(car translated-syms))
+                 (incf *instruction-pointer*))))))
 
-(defun generate-inc (args)
-  `(lambda ()
-     (incf (aref *registers* ,(translate-sym (car args))))
-     (incf *instruction-pointer*)))
+(defun generate-inc (instruction-index args)
+  `(,instruction-index
+     (progn (incf (aref *registers* ,(translate-sym (car args))))
+            (incf *instruction-pointer*))))
 
-(defun generate-dec (args)
-  `(lambda ()
-     (decf (aref *registers* ,(translate-sym (car args))))
-     (incf *instruction-pointer*)))
+(defun generate-dec (instruction-index args)
+  `(,instruction-index
+    (progn (decf (aref *registers* ,(translate-sym (car args))))
+           (incf *instruction-pointer*))))
 
-(defun generate-jnz (args)
+(defun generate-jnz (instruction-index args)
   (if (symbolp (car args))
-      `(lambda ()
+      `(,instruction-index
          (if (/= 0 (aref *registers* ,(translate-sym (car args))))
              (incf *instruction-pointer* ,(cadr args))
              (incf *instruction-pointer*)))
-      `(lambda ()
+      `(,instruction-index
          (if (/= 0 ,(car args))
              (incf *instruction-pointer* ,(cadr args))
              (incf *instruction-pointer*)))))
 
-(defun generate-instruction (syms)
+(defun generate-instruction (instruction-index syms)
   (case (car syms)
-    (cpy (generate-copy (cdr syms)))
-    (inc (generate-inc (cdr syms)))
-    (dec (generate-dec (cdr syms)))
-    (jnz (generate-jnz (cdr syms)))
+    (cpy (generate-copy instruction-index (cdr syms)))
+    (inc (generate-inc instruction-index (cdr syms)))
+    (dec (generate-dec instruction-index (cdr syms)))
+    (jnz (generate-jnz instruction-index (cdr syms)))
     (otherwise (error (format nil "unknown instruction name ~a" (car syms))))))
 
 (defun read-syms (line)
@@ -63,25 +63,26 @@
 (defun read-instructions ()
   (with-open-file (f "day12-input")
     (when f
-      (map 'vector #'identity
-           (loop for line = (read-line f nil nil)
-              while line 
-              for syms = (read-syms line)
-              collect (eval (generate-instruction syms)))))))
+      (let ((instructions 
+             (loop for line = (read-line f nil nil)
+                while line 
+                for syms = (read-syms line)
+                for instruction-index = 0 then (+ 1 instruction-index)
+                collect (generate-instruction instruction-index syms))))
+        `(loop while (< *instruction-pointer* ,(length instructions))
+            do ,(append '(case *instruction-pointer*) instructions)
+            finally (return (aref *registers* 0)))))))
 
-(defun interpret-program (instructions)
-  (loop while (< *instruction-pointer* (length instructions))
-     for instruction = (aref instructions *instruction-pointer*)
-     do (funcall instruction))
-  (aref *registers* 0))
+(defun def-answer1 ()
+  (compile (eval `(defun answer1 ()
+            (setf *instruction-pointer* 0)
+            (fill *registers* 0)
+            ,(read-instructions)))))
 
-(defun answer1 ()
-  (setf *instruction-pointer* 0)
-  (fill *registers* 0)
-  (interpret-program (read-instructions)))
-
-(defun answer2 ()
-  (setf *instruction-pointer* 0)
-  (fill *registers* 0)
-  (setf (aref *registers* 2) 1)
-  (interpret-program (read-instructions)))
+(defun def-answer2 ()
+  (compile (eval 
+            `(defun answer2 ()
+               (setf *instruction-pointer* 0)
+               (fill *registers* 0)
+               (setf (aref *registers* 2) 1)
+               ,(read-instructions)))))
