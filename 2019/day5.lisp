13ddd0b4f6e9c0ab08d9b419bfd6b4bff5d59a87
Henry Steere <henry.steere@gmail.com>
2019-12-05 08:21:23 +0200

    Day 5 2019

--- 2019/day5.lisp
+++ 2019/day5.lisp
@@ -1,12 +1,12 @@
 (ql:quickload :cl-ppcre)
 (ql:quickload :iterate)
 (ql:quickload :anaphora)
-(ql:quickload :ironclad)
 (ql:quickload :trivia)
-(ql:quickload :flexi-streams)
+(ql:quickload :alexandria)
 
 (defpackage :day5
-  (:use :cl :cl-ppcre :iterate :anaphora :ironclad :trivia :flexi-streams))
+  (:use :cl :cl-ppcre :iterate 
+        :anaphora :trivia :alexandria))
 
 (in-package :day5)
 
@@ -17,10 +17,6 @@
   (let (*read-eval*)
     (mapcar #'read-from-string (split sep line))))
 
-(defun find-hash (num)
-  (let* ((input (format nil "~a" num)))
-    (byte-array-to-hex-string (digest-sequence :md5 (string-to-octets input)))))
-
 (defun eval-value (mode v codes)
   (case mode
     (0 (aref codes v))
@@ -29,18 +25,20 @@
 (defun digit (n i)
   (mod (floor n (expt 10 i)) 10))
 
+(defmacro with-params (ind codes &rest body)
+  (with-gensyms (encoding m1 m2)
+    `(let* ((,encoding (aref ,codes ,ind))
+            (,m1 (digit ,encoding 2))
+            (,m2 (digit ,encoding 3))
+            (v1 (eval-value ,m1 (aref ,codes (+ ,ind 1)) ,codes))
+            (v2 (eval-value ,m2 (aref ,codes (+ ,ind 2)) ,codes)))
+       ,@body)))
+
 (defun binop (ind codes op)
-  (let* ((encoding (aref codes ind))
-         (m1 (digit encoding 2))
-         (m2 (digit encoding 3))
-         (v1 (eval-value m1 (aref codes (+ ind 1)) codes))
-         (v2 (eval-value m2 (aref codes (+ ind 2)) codes))
-         (dest (aref codes (+ ind 3))))
-    (format t "M1 ~a M2 ~a ARG1 ~a ARG2 ~a DEST ~a~%" m1 m2 v1 v2 dest)
-    (setf (aref codes (aref codes (+ ind 3)))
-          (funcall op v1 v2))
-    (format t "RESULT ~a~%" (aref codes (aref codes (+ ind 3))))
-    (+ ind 4)))
+  (with-params ind codes
+     (setf (aref codes (aref codes (+ ind 3)))
+           (funcall op v1 v2))
+      (+ ind 4)))
 
 (defun mult (ind codes)
   (binop ind codes #'*))
@@ -58,18 +56,46 @@
     (format t "~a~%" (eval-value mode (aref codes (+ ind 1)) codes))
     (+ ind 2)))
 
+(defun jit (ind codes)
+  (with-params ind codes
+               (if (/= v1 0) v2 (+ ind 3))))
+
+(defun jif (ind codes)
+  (with-params ind codes
+               (if (= v1 0) v2 (+ ind 3))))
+
+(defun lt (ind codes)
+  (with-params ind codes
+   (let ((dest (aref codes (+ ind 3))))
+     (if (< v1 v2) (setf (aref codes dest) 1) (setf (aref codes dest) 0))
+     (+ ind 4))))
+
+(defun equals (ind codes)
+  (with-params ind codes
+    (let ((dest (aref codes (+ ind 3))))
+     (if (= v1 v2) (setf (aref codes dest) 1) (setf (aref codes dest) 0))
+     (+ ind 4))))
+
 (defun find-termination (in codes)
   (iter (with i = 0)
         (for code = (aref codes i))
-        (format t "CODE ~a~%" code)
         (case (mod code 100)
           (99 (return-from find-termination nil))
           (1 (setf i (plus i codes)))
           (2 (setf i (mult i codes)))
           (3 (setf i (save in i codes)))
-          (4 (setf i (output i codes))))))
+          (4 (setf i (output i codes)))
+          (5 (setf i (jit i codes)))
+          (6 (setf i (jif i codes)))
+          (7 (setf i (lt i codes)))
+          (8 (setf i (equals i codes))))))
 
 (defun answer-1 ()
   (let ((codes (with-open-file (f "input5") 
                  (map 'vector #'identity (ints (read-line f))))))
     (find-termination 1 codes)))
+
+(defun answer-2 ()
+  (let ((codes (with-open-file (f "input5")
+                 (map 'vector #'identity (ints (read-line f))))))
+    (find-termination 5 codes)))
