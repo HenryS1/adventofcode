ed591cc4f50f5acd976ab21a60d6376ac8653db2
Henry Steere <henrysteere@Henrys-MacBook-Pro.local>
2024-12-18 08:45:09 +0200

    Day 17 2024 WIP

--- 2024/src/day17.lisp
+++ 2024/src/day17.lisp
@@ -63,7 +63,7 @@
                 (incf i 2))
                (3 (if (/= a 0)
                       (progn 
-                        (format t "A ~a B ~a C ~a OUTPUT ~a~%" a b c output)
+;                        (format t "A ~a B ~a C ~a OUTPUT ~a~%" a b c output)
                         (setf i operand))
                       (incf i 2)))
                (4 (setf b (logxor b c))
@@ -74,169 +74,62 @@
                     (setf b result)
                     (incf i 2)))
                (7 (let ((result (floor a (ash 1 (combo-operand operand a b c)))))
-                    (format t "A ~a C ~a OPERAND ~a COMBO OPERAND ~a DIV ~a RESULT ~a~%" a c operand (combo-operand operand a b c) (ash 1 (combo-operand operand a b c)) result)
+;                    (format t "A ~a C ~a OPERAND ~a COMBO OPERAND ~a DIV ~a RESULT ~a~%" a c operand (combo-operand operand a b c) (ash 1 (combo-operand operand a b c)) result)
                     (setf c result)
                     (incf i 2))))
-             (format t "INSTRUCTION ~a OPERAND ~a A ~a B ~a C ~a~%" instruction operand a b c)
+    ;         (format t "INSTRUCTION ~a OPERAND ~a A ~a B ~a C ~a~%" instruction operand a b c)
           finally (return output))))
 
 (defun part1 ()
   (let ((computer (read-input "day17input")))
-    (reverse (run-program computer))))
-
-(defun find-a (computer)
-  (let* ((init-b (computer-b computer))
-         (init-c (computer-c computer))
-         (program (computer-program computer))
-         (target (reverse (map 'list #'identity program))))
-    (loop with b = init-b
-          with c = init-c
-          for a-init from 0
-          for a = a-init
-          for iter-output = (loop with i = 0
-                             with output = nil
-                             while (< i (length program))
-                             for instruction = (aref program i)
-                             for operand = (aref program (+ i 1))
-                             do (case instruction
-                                  (0 (let ((result (floor a 
-                                                          (ash 1 (combo-operand operand a b c)))))
-                                       (setf a result)
-                                       (incf i 2)))
-                                  (1 (setf b (logxor b operand)) 
-                                   (incf i 2))
-                                  (2 (setf b (mod (combo-operand operand a b c) 8))
-                                   (incf i 2))
-                                  (3 (if (/= a 0)
-                                         (progn 
-                                           (setf i operand))
-                                         (incf i 2)))
-                                  (4 (setf b (logxor b c))
-                                   (incf i 2))
-                                  (5 (push (mod (combo-operand operand a b c) 8) output)
-                                   (incf i 2))
-                                  (6 (let ((result (floor a
-                                                          (ash 1 (combo-operand operand a b c)))))
-                                       (setf b result)
-                                       (incf i 2)))
-                                  (7 (let ((result (floor a
-                                                          (ash 1 (combo-operand operand a b c)))))
-                                       (setf c result)
-                                       (incf i 2))))
-                             finally (return output))
-;          do (format t "A ~a~%" a-init)
-          ;; when (= a-init 117440)
-          ;;   do (format t "EXPECTED OUTPUT ~a TARGET ~a~%" iter-output target)
-          ;; when (equal iter-output target)
-          do (format t "OUTPUT ~a TARGET ~a~%" iter-output target)
-          until (equal iter-output target)
-          finally (return a-init))))
+    (run-program computer)))
 
 (defun find-periodic-digits (computer)
-  (loop with increment = 1
-        for a = 1 then (+ a increment)
-        while (< increment (expt 8 16))
-        do (setf (computer-a computer) a)
-           (format t "OUTPUT ~a~%" (run-program computer))
-        when (= (mod a (* 8 increment)) 0)
-          do (setf increment (* 8 increment))))
+  (let ((target (reverse (computer-program computer))))
+    (loop with indices = nil
+          for target-index from 0
+          for target-number across target
+          for base-a = 1 then (+ base-a increment)
+          for increment = 7 then (* 8 increment)
+          for next-index = (loop for i from 0 
+                                 for index-diff = (loop for (i . increment-product)
+                                                          in indices 
+                                                        for j = increment-product 
+                                                          then (* j increment-product)
+                                                        do (format t "I ~a J ~a~%" i j)
+                                                        summing (* i j))
+                                 do (format t "INCREMENT ~a INDICES ~a~%" increment indices)
+                                    (format t "BASE A ~a INDEX ~a INDEX DIFF ~a~%" base-a i index-diff)
+                                    (setf (computer-a computer) (+ base-a i index-diff))
+                                    (let ((result (run-program computer)))
+                                      (format t "A ~a TARGET INDEX ~a TARGET NUMBER ~a OUTPUT ~a~%" 
+                                              (+ base-a i index-diff) target-index target-number result)
+                                      (setf (computer-a computer) (+ base-a i increment))
+                                      (format t "PLUS INCREMENT ~a ~a~%" 
+                                              (+ base-a i increment)
+                                              (run-program computer))
+                                      (when (= (nth target-index result) target-number)
+                                        (when (= target-index (- (length target) 1))
+                                          (format t "A ~a~%" (+ base-a i index-diff)))
+                                        (return i)))
+                                    (setf (computer-a computer) base-a))
+          do (push (cons next-index 8) indices)
+          do (format t "A ~a OUTPUT ~a~%" base-a (run-program computer))
+             ;; when (= (mod a (* 8 increment)) 0)
+             ;;   do (setf increment (* 8 increment))
+          finally (return indices))))
 
-(defun choose-one-from-each (lists)
-  (if (null lists)
-      (list nil)
-      (loop for fst in (car lists)
-            appending (loop for rest in (choose-one-from-each (cdr lists))
-                            collect (cons fst rest)))))
+(defun print-a-output ()
+  (let ((computer (read-input "day17input")))
+    (loop for a from 0 to 40000
+          do (setf (computer-a computer) a)
+             (format t "OUTPUT ~a~%" (run-program computer)))))
 
-(defun agree (one other another)
-  (and (= (logand (car one) 1) 
-          (ash (cdr other) -2))
-       (= (logand (car one) 3)
-          (ash (cdr another) -1))))
-
-(defun find-pair-agreement-2 (pair-lists target)
-  (format t "PAIR LISTS ~a~%" pair-lists)
-  (labels ((rec (remaining result len)
-             (format t "REMAINING ~a~%" remaining)
-             (if (null remaining)
-                 (reverse result)
-                 (loop for other in (car remaining)
-                       for a-so-far = (make-a (reverse (cons other result)))
-                       for digits = (find-digits a-so-far len)
-                       do (format t "ACC ~a A SO FAR ~a DIGITS ~a TARGET ~a~%" (cons other result) a-so-far digits target)
-                       when (every #'= digits target)
-                         do (let ((rest (rec (cdr remaining) (cons other result) (+ len 1))))
-                              (when rest (return rest)))))))
-    (loop for one in (car pair-lists)
-          for result = (rec (cdr pair-lists) (list one) 1)
-                       ;; (loop for other in (cadr pair-lists)
-                       ;;       for inner-result = (rec one other (cdr pair-lists) (list one))
-                       ;;       when inner-result do (return inner-result))
-          when result
-            do (return result))
-    ))
-
-(defun find-pair-agreement (pair-lists)
-  (labels ((rec (one other remaining result)
-             (format t "REMAINING ~a~%" remaining)
-             (if (null remaining)
-                 (reverse result)
-                 (loop for another in (car remaining)
-                       when (agree one other another)
-                         do (return (rec other another (cdr remaining) (cons other result)))))))
-    (loop for one in (car pair-lists)
-          for result = (loop for other in (cadr pair-lists)
-                             for inner-result = (rec one other (cdr pair-lists) (list one))
-                             when inner-result do (return inner-result))
-          when result
-            do (return result))))
-
-(defun pair-to-number (pair offset)
-  (logior (ash (car pair) (+ (logxor (cdr pair) 5) offset))
-          (ash (cdr pair) offset)))
-
-(defun make-a (pairs)
-  (loop with a = 0
-        for pair in pairs 
-        for offset = 0 then (+ offset 3)
-        do (setf a (logior a (pair-to-number pair offset)))
-        finally (return a)))
-
-(defun pair-less (one other)
-  (or (< (car one) (car other))
-      (and (= (car one) (car other))
-              (< (cdr one) (cdr other)))))
-
-(defun find-potential-pairs (computer)
-  (loop for n across (computer-program computer)
-        collect (loop for i from 0 to 7 
-                      appending (loop for j from 0 to 7
-                                      when (= (logxor i j 3) n)
-                                        collect (cons i j)))))
-
-(defun compute-a (computer)
-  (let ((pairs (find-potential-pairs computer)))
-    pairs
-;    (find-pair-agreement pairs)
-    ))
-
-(defun find-digits (a n)
-  (loop with digits = nil
-        with b = 0
-        with c = 0
-        for i from 1 to n
-        do (setf b (mod a 8))
-           (setf b (logxor b 5))
-           (setf c (ash a (- b)))
-           (setf a (floor a 8))
-           (setf b (logxor b c))
-           (setf b (logxor b 6))
-        do (push (mod b 8) digits)
-           (format t "A ~a B ~a C ~a OUTPUT ~a~%" a b c digits)
-        while (> a 0)
-        finally (return digits)))
+(defun run-with-a (a)
+  (let* ((computer (read-input "day17input")))
+    (setf (computer-a computer) a)
+    (run-program computer)))
 
 (defun part2 ()
-  (let* ((computer (read-input "day17input"))
-         (pairs (find-potential-pairs computer)))
-    (find-pair-agreement-2 pairs (computer-program computer))))
+  (let* ((computer (read-input "day17input")))
+    (find-periodic-digits computer)))
