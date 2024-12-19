0d44f3a3232064d1dc900251f32fb817c1b46024
Henry Steere <henrysteere@Henrys-MacBook-Pro.local>
2024-12-19 18:21:39 +0200

    Optimize day 19 2024

--- 2024/src/day19.lisp
+++ 2024/src/day19.lisp
@@ -20,7 +20,7 @@
         *read-eval*)
     (with-input-from-string (s without-comma)
       (loop for symbol = (read s nil nil)
-            while symbol collect (string-downcase (symbol-name symbol))))))
+            while symbol collect (map 'string #'char-downcase (symbol-name symbol))))))
 
 (defun read-input (file)
   (with-open-file (f file)
@@ -30,10 +30,10 @@
             while line collect line into patterns
             finally (return (cons towels patterns))))))
 
-(defun ordered-already-seen-patterns (already-seen)
-  (sort (hash-table-keys already-seen) (lambda (a b) (> (length a) (length b)))))
-
+(declaim (inline prefix-matches))
 (defun prefix-matches (candidate pattern start-index end-index)
+  (declare (optimize (speed 3)) ((simple-array character) candidate pattern) 
+           (fixnum start-index end-index))
   (when (>= (- end-index start-index) (length candidate))
     (loop for one across candidate
           for i from start-index to (- end-index 1)
@@ -41,17 +41,19 @@
             do (return nil)
           finally (return t))))
 
-(defun pattern-is-possible (towels pattern start-index end-index candidates impossible)
+(defun pattern-is-possible (towels pattern start-index end-index impossible)
   (let ((key (make-array (- end-index start-index) :element-type 'character
                          :displaced-to pattern :displaced-index-offset start-index)))
+    (declare (optimize (speed 3)) (fixnum start-index end-index) 
+             ((simple-array character) pattern) ((array character) key))
     (and (not (gethash key impossible))
          (or (= start-index end-index)
-             (loop for candidate in candidates
-                   for i = (length candidate)
+             (loop for candidate of-type string in towels
+                   for i fixnum = (length candidate)
                    when (and (<= (length candidate) (- end-index start-index))
                              (prefix-matches candidate pattern start-index (+ i start-index))
                              (pattern-is-possible towels pattern (+ i start-index)
-                                                  end-index candidates impossible))
+                                                  end-index impossible))
                      do (return t)
                    finally (progn (setf (gethash key impossible) t)
                                   (return nil)))))))
@@ -65,18 +67,19 @@
           counting (pattern-is-possible 
                     towels pattern 0
                     (length pattern) 
-                    towels
                     impossible))))
 
 (defun ways-to-make (towels pattern start-index end-index impossible ways-to-make)
-  (let* ((key (make-array (- end-index start-index) :element-type 'character
+  (let* ((key (make-array (the (unsigned-byte 32)
+                               (- end-index start-index)) :element-type 'character
                          :displaced-to pattern :displaced-index-offset start-index))
          (ways 0)
          (lookup (gethash key ways-to-make)))
+    (declare (optimize (speed 3)) (fixnum ways start-index end-index) ((array character) key))
     (or lookup
         (and (not (gethash key impossible))
              (or (and (= start-index end-index) 1)
-                 (loop for candidate in towels
+                 (loop for candidate of-type string in towels
                        for i = (length candidate)
                        for new-ways = (and (<= (length candidate) (- end-index start-index))
                                            (prefix-matches candidate pattern start-index 
@@ -84,7 +87,9 @@
                                            (ways-to-make towels pattern (+ i start-index)
                                                          end-index impossible ways-to-make))
                        when new-ways
-                         do (incf ways new-ways)
+                         do (let ((value new-ways))
+                              (declare (fixnum value))
+                              (incf ways value))
                        finally (if (= ways 0)
                                    (setf (gethash key impossible) t)
                                    (setf (gethash key ways-to-make) ways))
